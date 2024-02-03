6aba07fb8a418d35975de5549b204acb9fc16f4e
Kirubel2001 <109730701+Kirubel2001@users.noreply.github.com>
2024-02-03 18:08:21 +0300

    commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ const Footer =() =>{
     return(
      <div className="social-image">
         <img src="..\..\Social-media.jpeg" className="social-media"/>
+        <h1></h1>
      </div>
 
     );
